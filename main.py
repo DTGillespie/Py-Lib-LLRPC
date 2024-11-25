c9e03d157d4d318fd556a095adae441b8135ba5d
Dylan Gillespie <dylantannergillespie@gmail.com>
2024-11-25 15:46:22 -0500

    Initial commit; implementing FFI

---
+++ main.py
@@ -0,0 +1,55 @@
+import ctypes
+import os
+from ctypes import c_char_p, c_int, c_void_p, POINTER
+
+lib_path = os.path.join(os.path.dirname(__file__), "llrp_client.dll")
+llrp_lib = ctypes.CDLL(lib_path)
+
+# Define argument and return types for exposed functions
+llrp_lib.get_last_error.restype = c_char_p
+
+llrp_lib.initialize_client.argtypes = [c_char_p]
+llrp_lib.initialize_client.restype = POINTER(c_void_p)
+
+llrp_lib.send_keep_alive.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_keep_alive.restype = c_int
+
+llrp_lib.send_enable_events_and_reports.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_enable_events_and_reports.restype = c_int
+
+llrp_lib.send_get_reader_capabilities.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_get_reader_capabilities.restype = c_int
+
+llrp_lib.send_get_reader_config.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_get_reader_config.restype = c_int
+
+llrp_lib.send_set_reader_config.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_set_reader_config.restype = c_int
+
+llrp_lib.send_add_rospec.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_add_rospec.restype = c_int
+
+llrp_lib.send_enable_rospec.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_enable_rospec.restype = c_int
+
+llrp_lib.send_start_rospec.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_start_rospec.restype = c_int
+
+llrp_lib.send_stop_rospec.argtypes = [POINTER(c_void_p)]
+llrp_lib.send_stop_rospec.restype = c_int
+
+llrp_lib.send_delete_rospec.argtypes = [POINTER(c_void_p), c_int]
+llrp_lib.send_delete_rospec.restype = c_int
+
+ROAccessReportCallback = ctypes.CFUNCTYPE(None, c_char_p)
+llrp_lib.set_ro_access_report_callback.argtypes = [ROAccessReportCallback]
+llrp_lib.set_ro_access_report_callback.restype = None
+
+llrp_lib.await_ro_access_report.argtypes = [POINTER(c_void_p)]
+llrp_lib.await_ro_access_report.restype = c_int
+
+llrp_lib.disconnect_client.argtypes = [POINTER(c_void_p)]
+llrp_lib.disconnect_client.restype = None
+
+llrp_lib.free_client.argtypes = [POINTER(c_void_p)]
+llrp_lib.free_client.restype = None
